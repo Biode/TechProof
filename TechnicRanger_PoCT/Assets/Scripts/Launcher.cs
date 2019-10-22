009fadc13fdd7176ca4b476c51e538be39bed2eb
Caniheal <37191567+Caniheal@users.noreply.github.com>
2019-10-22 11:30:12 -0400

    Anchor protypeV2

---
+++ TechnicRanger_PoCT/Assets/Scripts/Launcher.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField]
+    private Transform firePoint;
+    [SerializeField]
+    private Transform spawnPoint;
+    [SerializeField]
+    private Rigidbody projectilePrefab;
+    [SerializeField]
+    private Rigidbody platformPrefab;
+    [SerializeField]
+    private float launchForce = 700f;
+
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            LaunchProjectile();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            SpawnPlatform();
+        }
+                
+
+    }
+
+    private void LaunchProjectile()
+    {
+        
+            var projectileInstance = Instantiate(
+                projectilePrefab,
+                firePoint.position,
+                firePoint.rotation);
+
+            projectileInstance.AddForce(firePoint.forward * launchForce);
+        
+    }
+    private void SpawnPlatform()
+    {
+
+        var platformInstance = Instantiate(
+            platformPrefab,
+            spawnPoint.position,
+            firePoint.rotation);
+
+       
+
+    }
+}
