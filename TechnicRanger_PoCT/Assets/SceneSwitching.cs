077a467e72b620df067e299920a9a0a3b5e2b688
luci-fa <51527595+luci-fa@users.noreply.github.com>
2019-10-23 02:31:58 +1100

    Scene Switching
    Start programming

---
+++ TechnicRanger_PoCT/Assets/SceneSwitching.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("TechProof"));
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Skateboard"));
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Whitebox"));
+        }
+    }
+}
